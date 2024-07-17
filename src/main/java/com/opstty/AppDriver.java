f0dbca991c5ab2da1b341ed92c0252831c5383ff
Nevr-Git <never4killu@gmail.com>
2024-07-17 17:33:32 +0200

    added "distinct" job

--- src/main/java/com/opstty/AppDriver.java
+++ src/main/java/com/opstty/AppDriver.java
@@ -1,6 +1,7 @@
 package com.opstty;
 
 import com.opstty.job.WordCount;
+import com.opstty.job.Distinct;
 import org.apache.hadoop.util.ProgramDriver;
 
 public class AppDriver {
@@ -11,6 +12,8 @@ public class AppDriver {
         try {
             programDriver.addClass("wordcount", WordCount.class,
                     "A map/reduce program that counts the words in the input files.");
+            programDriver.addClass("distinct", Distinct.class,
+                    "A map/reduce program that outputs the distinct tree names in the input files.");
 
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
