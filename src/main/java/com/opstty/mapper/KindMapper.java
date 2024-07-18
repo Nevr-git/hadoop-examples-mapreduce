7dd4a2d924a8a211aba8f31f766968f2af24bb48
Nevr <never4killu@gmail.com>
2024-07-18 23:47:03 +0200

    fixed "kindcount" job, (mapper[3] type Text -> IntWritable)

--- src/main/java/com/opstty/mapper/KindMapper.java
+++ src/main/java/com/opstty/mapper/KindMapper.java
@@ -1,14 +1,16 @@
 package com.opstty.mapper;
 
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class KindMapper extends Mapper<LongWritable, Text, Text, Text> {
+public class KindMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
     private Text districtName = new Text();
     private static boolean isFirstLine = true;
+    private final static IntWritable one = new IntWritable(1);
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
@@ -24,7 +26,7 @@ public class KindMapper extends Mapper<LongWritable, Text, Text, Text> {
         // Assuming the tree name is in the first column, adjust index if necessary
         if (fields.length > 3) {
             districtName.set(fields[3].trim());
-            context.write(districtName, new Text(""));
+            context.write(districtName, one);
         }
     }
 }
